2de6f80d3b51ffb8eb52ac6bb97939fb091cb9d9
rocamocha <rocamocha@cyootlabs.net>
2021-07-25 19:04:44 -1000

    color conversion now uses existing alpha value

--- printer/colors.lua
+++ printer/colors.lua
@@ -9,10 +9,11 @@ end
 function M.hex2rgba(hex) -- normalized
     hex = string.gsub(hex, "#", "")
     local color
+    local w = gui.get_color("printer/prefab").w
     if(string.len(hex) == 3) then
-        color = vmath.vector4(tonumber("0x"..string.sub(hex, 1,1)) * 17, tonumber("0x"..string.sub(hex, 2,2)) * 17, tonumber("0x"..string.sub(hex, 3,3)) * 17, 1.0)
+        color = vmath.vector4(tonumber("0x"..string.sub(hex, 1,1)) * 17, tonumber("0x"..string.sub(hex, 2,2)) * 17, tonumber("0x"..string.sub(hex, 3,3)) * 17, w)
     elseif(string.len(hex) == 6) then
-        color = vmath.vector4(tonumber("0x"..string.sub(hex, 1,2)), tonumber("0x"..string.sub(hex, 3,4)), tonumber("0x"..string.sub(hex, 5,6)), 1.0)
+        color = vmath.vector4(tonumber("0x"..string.sub(hex, 1,2)), tonumber("0x"..string.sub(hex, 3,4)), tonumber("0x"..string.sub(hex, 5,6)), w)
     end
     color.x = color.x / 255
     color.y = color.y / 255
