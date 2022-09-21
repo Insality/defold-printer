0abd6ee2177845444071580e9b032bbd4bae8cab
rocamocha <rocamocha@cyootlabs.net>
2022-09-20 14:51:43 -1000

    cleaned up new features, updated docs

--- printer/printer.lua
+++ printer/printer.lua
@@ -102,18 +102,23 @@ local function update_shake(self, dt)
 	end
 end
 
-
 local function get_style(self, name)
 	if name == "/" or name == "default" or name == nil then
 		if self.default_style == "default" then
+            self.last_style = styles.default
 			return styles.default
 		else
 			name = self.default_style
+            self.last_style = styles[name]
 		end
 	end
 
 	local style = {}
 	for k, v in pairs(styles.default) do
+		style[k] = v
+	end
+
+    for k, v in pairs(self.last_style) do
 		style[k] = v
 	end
 
@@ -125,6 +130,7 @@ local function get_style(self, name)
 		print("[Printer]: No style name " .. name)
 	end
 
+    self.last_style = style
 	return style
 end
 
@@ -148,8 +154,8 @@ local function get_letter_size(node_data)
 
 	if size.width == 0 then
 		-- possibly whitespace
-		size.width = size.max_ascent/2
-		size.height = size.max_ascent/2
+		size.width = size.max_ascent * 0.5
+		size.height = size.max_ascent * 0.5
 	end
 
 	size.width = size.width * scale.x
@@ -180,7 +186,7 @@ local function check_new_line(self, word)
 
 	if self.prev_node then
 		local last_pos = gui.get_position(self.prev_node.node)
-		if last_pos.x + word_size >= self.parent_size.x/2 then
+		if last_pos.x + word_size >= self.parent_size.x * 0.5 then
 			self.new_row = true
 		end
 	else
@@ -204,10 +210,11 @@ local function update_letter_pos(self, node_data)
 
 	if not self.prev_node or is_new_row then
 		-- first symbol
-		pos.x = -self.parent_size.x/2
+		pos.x = -self.parent_size.x * 0.5
 		pos.y = self.parent_size.y - ((self.current_row-1) * style.font_height) - style.font_height
         if is_new_row then
-            pos.y = pos.y - style.line_spacing * (self.current_row-1)
+            local line_spacing = style.line_spacing or style.font_height * 0.5
+            pos.y = pos.y - line_spacing * (self.current_row-1)
         end
 	else
 		local prev_pos = gui.get_position(self.prev_node.node)
@@ -350,7 +357,7 @@ local function appear_node(self, node_data, is_instant)
 		M.play_sound(style.sound)
 	end
 
-	local color = colors.hex2rgba(style.color)
+	local color = colors.hex2rgba(self, style.color)
 	if node_data.is_icon then
 		color = vmath.vector4(1)
 	end
@@ -360,6 +367,7 @@ local function appear_node(self, node_data, is_instant)
 		color.w = 1
 		gui.animate(node, gui.PROP_COLOR, color, gui.EASING_OUTSINE, style.appear)
 	else
+        color.w = 1
 		gui.set_color(node, color)
 	end
 
@@ -442,15 +450,13 @@ end
 
 --== PUBLIC FUNCTIONS ==--
 
----< function that fades the printer instance out
--- author: rocamocha
 function M.fadeout(self)
 	self.is_print = false
 	for i = 1, #self.current_letters do
 		local node = self.current_letters[i].node
 		gui.animate(node, 'color.w', 0, gui.EASING_LINEAR, 0.3)
 	end
-end -->
+end
 
 function M.instant_appear(self)
 	local current_letter = self.current_letters[self.current_index]
@@ -476,6 +482,7 @@ function M.print(self, str, source)
 		self.new_row = false
 		self.stylename = source_styles[source] or "default"
 		self.default_style = self.stylename
+        self.last_style = styles[self.default_style]
 		self.prev_node = false
 		clear_prev_text(self)
 		self.string = str
@@ -528,7 +535,7 @@ end
 
 
 function M.play_sound(name)
-	-- rewrite
+	-- this function is called whenever a symbol is printed
 end
 
 
